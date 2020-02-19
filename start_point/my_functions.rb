6e6fbb20118a21d32477ec3158ed9231ae0bb6bd
CameronTait <cameron-tait@hotmail.com>
2020-02-19 16:10:20 +0000

    adding lab files

---
+++ start_point/my_functions.rb
@@ -0,0 +1,40 @@
+# def add_length_of_arrays(citrus_fruit, berrie)
+#   length = 0
+#   for citrus_fruit in citrus_fruits
+#   citrus_fruits.length
+# end
+#
+# for berrie in berries
+# berries.length
+# end
+#
+# length = citrus_fruit + berrie
+#
+#     return result
+#
+#
+# end
+
+
+def add_array_lengths(array1, array2)
+  # length = 0
+  # for length in array1
+  #   return array1.length
+  # end
+  # for length in array2
+  #   return array2.length
+  # end
+  # result = array1 + array2
+  result = array1.length + array2.length
+  return result
+end
+
+
+def sum_array(numbers)
+  total = 0
+  for num in numbers
+    total += num
+  end
+  return total
+
+end
